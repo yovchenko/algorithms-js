df12a83df73ab6024d9fd4ab9e9543016101c4d4
Vladymyr <yovchenko.w@gmail.com>
2017-05-17 23:00:53 +0300

    initial

--- js/additional.js
+++ js/additional.js
@@ -1,24 +1,35 @@
 
 
 function prn(value) {
-    if (value===0){
-        document.write('RESULT:'+'<br>'+value);
+    let x = 0;
+    if (x===0) {
+        this.document.write('RESULT:'+'<br>'+value[0]);
+         x++;
     }
-    else {
-        document.write('<br>'+value);
+    for (x === 1; x < value.length;) {
+    this.document.write('<br>'+value[x]);
+         x++;
     }
 }
 
 function count(num) {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 1; i++) {
       link(i);
     }
 }
 
 function link(init) {
-    if ( init > 50 ) {
-     prn(init);
+    let y = [];
+    y[0] = 'A';
+    y[1] = 'B';
+    y[2] = 'C';
+    if ( init < y.length) {
+     prn(y);
     }
 }
 
 var start = count();
+
+
+
+
