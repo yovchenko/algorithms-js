73096ffae2436b2fd0428da8e6c6513b03a2deac
yovchenko <volodymyr.yovchenko@gmail.com>
2020-12-27 20:46:33 +0200

    feat(server): create deep clo\ne function

--- client/src/ts/index.ts
+++ client/src/ts/index.ts
@@ -1,8 +1,18 @@
 import "../scss/index.scss";
 import "bootstrap";
+
 document.getElementById("header-button").addEventListener("click", function(e) {
   e.stopPropagation();
   const temp = document.getElementsByTagName("template")[0];
   const clon = temp.content.cloneNode(true);
   document.getElementsByTagName("main")[0].appendChild(clon);
 });
+
+document
+  .getElementsByTagName("main")[0]
+  .addEventListener("click", mainBlockClick);
+
+function mainBlockClick(e: Event): void {
+  console.log(e.target);
+  this.classList.toggle("test");
+}
--- server/src/ts/index.ts
+++ server/src/ts/index.ts
@@ -16,6 +16,7 @@ function isIsogram(str: string): boolean {
 console.log(isIsogram("Dermatoglyphics"));
 */
 
+/*
 export function duplicateCount(text: string): number {
   text = text.toLowerCase();
   const len = text.length;
@@ -34,3 +35,43 @@ export function duplicateCount(text: string): number {
   return count;
 }
 //console.log(duplicateCount("aabbcde"));
+*/
+const animal = {
+  eats: true,
+  walk() {
+    alert("Animal walk");
+  }
+};
+
+const rabbit = {
+  jumps: true,
+  __proto__: animal
+};
+
+const longEar = {
+  earLength: 10,
+  __proto__: rabbit
+};
+
+const deepCopyFunction = inObject => {
+  let value, key;
+
+  if (typeof inObject !== "object" || inObject === null) {
+    return inObject; // Return the value if inObject is not an object
+  }
+
+  // Create an array or object to hold the values
+  const outObject = Array.isArray(inObject) ? [] : {};
+
+  for (key in inObject) {
+    value = inObject[key];
+
+    // Recursively (deep) copy for nested objects, including arrays
+    outObject[key] = deepCopyFunction(value);
+  }
+
+  return outObject;
+};
+
+console.log(Object.getOwnPropertyNames(longEar));
+console.log(deepCopyFunction(longEar));
