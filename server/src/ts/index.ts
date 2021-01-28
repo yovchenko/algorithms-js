a166116bc3e26cb8abe6b8703ac844e0d8896e9a
yovchenko <volodymyr.yovchenko@gmail.com>
2021-01-28 21:03:29 +0200

    feat(server): create Product factory

--- server/src/ts/index.ts
+++ server/src/ts/index.ts
@@ -1,5 +1,118 @@
+/**
+ * The Product interface declares the operations that all concrete products must
+ * implement.
+ */
+interface Product {
+  operation(): string;
+}
+
+/**
+ * Concrete Products provide various implementations of the Product interface.
+ */
+class ConcreteProduct1 implements Product {
+  public operation(): string {
+    return "{Result of the ConcreteProduct1}";
+  }
+}
+
+class ConcreteProduct2 implements Product {
+  public operation(): string {
+    return "{Result of the ConcreteProduct2}";
+  }
+}
+
+/**
+ * The Creator class declares the factory method that is supposed to return an
+ * object of a Product class. The Creator's subclasses usually provide the
+ * implementation of this method.
+ */
+abstract class Creator {
+  /**
+   * Note that the Creator may also provide some default implementation of the
+   * factory method.
+   */
+  public abstract factoryMethod(): Product;
+
+  /**
+   * Also note that, despite its name, the Creator's primary responsibility is
+   * not creating products. Usually, it contains some core business logic that
+   * relies on Product objects, returned by the factory method. Subclasses can
+   * indirectly change that business logic by overriding the factory method
+   * and returning a different type of product from it.
+   */
+  public someOperation(): string {
+    // Call the factory method to create a Product object.
+    const product = this.factoryMethod();
+    // Now, use the product.
+    return `Creator: The same creator's code has just worked with ${product.operation()}`;
+  }
+}
+
+/**
+ * Concrete Creators override the factory method in order to change the
+ * resulting product's type.
+ */
+class ConcreteCreator1 extends Creator {
+  /**
+   * Note that the signature of the method still uses the abstract product
+   * type, even though the concrete product is actually returned from the
+   * method. This way the Creator can stay independent of concrete product
+   * classes.
+   */
+  public factoryMethod(): Product {
+    return new ConcreteProduct1();
+  }
+}
+
+class ConcreteCreator2 extends Creator {
+  public factoryMethod(): Product {
+    return new ConcreteProduct2();
+  }
+}
+
+/**
+ * The client code works with an instance of a concrete creator, albeit through
+ * its base interface. As long as the client keeps working with the creator via
+ * the base interface, you can pass it any creator's subclass.
+ */
+function clientCode(creator: Creator): void {
+  // ...
+  console.log(
+    "Client: I'm not aware of the creator's class, but it still works."
+  );
+  console.log(creator.someOperation());
+  // ...
+}
+
+/**
+ * The Application picks a creator's type depending on the configuration or
+ * environment.
+ */
+console.log("App: Launched with the ConcreteCreator1.");
+clientCode(new ConcreteCreator1());
+console.log("");
+
+console.log("App: Launched with the ConcreteCreator2.");
+clientCode(new ConcreteCreator2());
+
+/*
 //import { interval, Observable } from "rxjs";
 // import { map } from "rxjs/operators";
+import * as _ from "lodash";
+
+const array = [
+  { dir: "left", code: 99 },
+  { dir: "right", code: 100 },
+  { dir: "left", code: 98 }
+];
+
+// => { 'a': { 'dir': 'left', 'code': 97 }, 'd': { 'dir': 'right', 'code': 100 } }
+
+const res = _.keyBy(array, "dir");
+// => { 'left': { 'dir': 'left', 'code': 97 }, 'right': { 'dir': 'right', 'code': 100 } }
+
+console.log(res);
+*/
 
 /*
 function isIsogram(str: string): boolean {
@@ -15,6 +128,7 @@ function isIsogram(str: string): boolean {
 console.log(isIsogram("Dermatoglyphics"));
 */
 
+/*
 export function duplicateCount(text: string): number {
   text = text.toLowerCase();
   const len = text.length;
@@ -116,6 +230,16 @@ function loggingIdentity<T extends Lengthwise>(arg: T): T {
 }
 
 loggingIdentity([1, 2, 3]);
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+const x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, "a");
+*/
+
 /*
 function* generateDoubles(seed) {
   let i = seed;
